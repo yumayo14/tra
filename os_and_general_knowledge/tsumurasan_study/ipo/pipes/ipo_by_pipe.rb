dbfc23cc4216be5de8577aa8204cd569147e2ddb
yumayo14 <yumayo14@gmail.com>
2021-03-13 17:58:09 +0900

    ipo 周りのディレクトリ構造を変更

---
+++ os_and_general_knowledge/tsumurasan_study/ipo/pipes/ipo_by_pipe.rb
@@ -0,0 +1,13 @@
+reader, writer = IO.pipe
+fork do
+  reader.close
+  10.times do
+    # 力仕事
+    writer.puts "Another one bites the dust"
+  end
+end
+
+writer.close
+while message = reader.gets
+  $stdout.puts message
+end
